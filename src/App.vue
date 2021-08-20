d362566c200140c770ea4f76d122b99648de7b5a
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-08-20 19:02:37 +0100

    add new file

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <user-information />
+  </div>
+</template>
+
+<script>
+import UserInformation from './components/UserInformation.vue'
+export default {
+  name: 'App',
+  components: {
+    UserInformation
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/UserInformation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="userInfo">
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInformation'
+}
+</script>
+
+<style scoped>
+.userInfo {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  margin-top: 25%;
+  background: #000;
+  padding: 10px 30px;
+}
+</style>
